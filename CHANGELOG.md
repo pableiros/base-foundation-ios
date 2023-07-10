7358aa68cb699b8e1368ccb3dea846432f961f10
Pablo Bórquez Urquidy <pablo.borquez.urquidy@gmail.com>
2023-07-10 16:08:00 -0700

    ErrorAsyncHandler implemented

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# 0.6.0
+
+- `ErrorAsyncHandler` implemented.
+
 # 0.5.0
 
 - `ErrorHandler` implemented.
---
+++ Sources/BaseFoundationiOS/Templates/Types/ErrorAsyncHandler.swift
@@ -0,0 +1,10 @@
+//
+//  ErrorAsyncHandler.swift
+//
+//
+//  Created by pablo borquez on 10/07/23.
+//
+
+import Foundation
+
+public typealias ErrorAsyncHandler =  (Error) async -> Void
