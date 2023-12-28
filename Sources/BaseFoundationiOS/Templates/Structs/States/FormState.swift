a9ecbd9a56738136ca3e3c342bae39e1a1512e22
Pablo Bórquez Urquidy <pablo.borquez.urquidy@gmail.com>
2023-12-27 17:48:49 -0700

    FormState added

--- Sources/BaseFoundationiOS/Templates/Structs/States/FormState.swift
+++ Sources/BaseFoundationiOS/Templates/Structs/States/FormState.swift
@@ -1,8 +1,28 @@
 //
-//  File.swift
-//  
+//  FormState.swift
+//
 //
 //  Created by pablo borquez on 20/12/23.
 //
 
 import Foundation
+
+public struct FormState {
+    public var isLoading = false
+    public var isFormInvalidPresented = false
+    public var errorMessage: String = "" {
+        didSet {
+            if self.errorMessage.isFullEmpty() == false {
+                self.isFormInvalidPresented.toggle()
+            }
+        }
+    }
+    
+    public init(isLoading: Bool = false,
+                isFormInvalidPresented: Bool = false,
+                errorMessage: String = "") {
+        self.isLoading = isLoading
+        self.isFormInvalidPresented = isFormInvalidPresented
+        self.errorMessage = errorMessage
+    }
+}
