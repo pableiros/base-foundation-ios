869d63654aa94020fb3145adf92c2354e5e976c5
Pablo Bórquez Urquidy <pablo.borquez.urquidy@gmail.com>
2025-01-05 13:15:29 -0700

    typo fixed

--- Sources/BaseFoundationiOS/Protocols/Delegates/FirestoreChainModelDelegate.swift
+++ Sources/BaseFoundationiOS/Protocols/Delegates/FirestoreChainModelDelegate.swift
@@ -13,7 +13,7 @@ public protocol FirestoreChainModelDelegate {
 extension FirestoreChainModelDelegate {
     public func handleChain<Container, ChainType>(chainType: ChainType.Type) async throws -> [Container] where ChainType: FirestoreChain<[Container]> {
         let chainArray = Array(repeating: chainType, count: 2)
-        let chain = FirestoreChain<Container>.createChain(from: chainArray)
+        let chain = FirestoreChain.createChain(from: chainArray)
         let categorias = try await chain?.handle() ?? [Container]()
         
         return categorias
